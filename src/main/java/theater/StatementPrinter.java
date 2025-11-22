a29cefc5f0d9e8b791df4e5398723dc627202532
jadenal-aidroos <jjtortoise@icloud.com>
2025-11-22 03:02:08 -0500

    Task 2.4

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -22,25 +22,41 @@ public class StatementPrinter {
      * @throws RuntimeException if one of the play types is not known
      */
     public String statement() {
-        int totalAmount = 0;
-        int volumeCredits = 0;
+
         final StringBuilder result = new StringBuilder("Statement for "
                 + invoice.getCustomer() + System.lineSeparator());
 
         for (Performance performance : invoice.getPerformances()) {
             final int thisAmount = getAmount(performance);
-
-            // add volume credits
-            volumeCredits = getVolumeCredits(performance, volumeCredits);
-
             // print line for this order
             result.append(String.format("  %s: %s (%s seats)%n", getPlay(performance).getName(),
                     usd(thisAmount), performance.getAudience()));
+        }
+
+        result.append(String.format("Amount owed is %s%n", usd(getTotalAmount())));
+        result.append(String.format("You earned %s credits%n", getTotalVolumeCredits()));
+        return result.toString();
+    }
+
+    private int getTotalAmount() {
+        int totalAmount = 0;
+
+        for (Performance performance : invoice.getPerformances()) {
+            final int thisAmount = getAmount(performance);
             totalAmount += thisAmount;
         }
-        result.append(String.format("Amount owed is %s%n", usd(totalAmount)));
-        result.append(String.format("You earned %s credits%n", volumeCredits));
-        return result.toString();
+        return totalAmount;
+    }
+
+    private int getTotalVolumeCredits() {
+        int volumeCredits = 0;
+
+        for (Performance performance : invoice.getPerformances()) {
+            // add volume credits
+            volumeCredits = getVolumeCredits(performance, volumeCredits);
+
+        }
+        return volumeCredits;
     }
 
     private static String usd(int totalAmount) {
